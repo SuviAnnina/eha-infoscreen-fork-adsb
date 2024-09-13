b203020e5c3a01c5c8f7ab01aac20f9f521cffbe
kridesav <savolainenkristjan@gmail.com>
2024-09-13 16:48:37 +0300

    wind-indicator + hoverable tooltips

--- src/components/runwayTemp.js
+++ src/components/runwayTemp.js
@@ -5,6 +5,7 @@ import styles from './runwayTemp.module.css';
 
 export default function RunwayTemp() {
     const [data, setData] = useState([]);
+    const [windData, setWindData] = useState({ knots: 3, angle: 130 }); // Mock data
 
     useEffect(() => {
         async function fetchData() {
@@ -14,13 +15,13 @@ export default function RunwayTemp() {
 
                 const stationsData = Object.keys(result.stations).map(stationId => {
                     const station = result.stations[stationId][0];
-                    const site = station[8][0]; 
+                    const site = station[8][0];
                     return {
                         id: stationId,
                         name: station[1],
                         temp: station[6],
                         condition: station[5],
-                        siteId: site.site_id, 
+                        siteId: site.site_id,
                     };
                 });
 
@@ -42,9 +43,34 @@ export default function RunwayTemp() {
         return station ? `${station.temp}°C` : 'N/A';
     };
 
+    const getStationBySiteId = (siteId) => {
+        return data.find(station => station.siteId === siteId);
+    };
+
+    const getColorAndExplanationByCondition = (condition) => { // TODO - add more conditions
+        switch (condition) {
+            case 1:
+                return { color: 'Green', explanation: 'Dry' };
+            case 13:
+                return { color: 'White', explanation: 'Not dry' };
+            default:
+                return { color: 'black', explanation: 'N/A' };
+        }
+    };
+
     return (
         <div className={styles.fidgetContainer}>
             <div className={styles.fidget}>
+                <div className={styles.windIndicator}>
+                    <div className={styles.windArrow}
+                        style={{ transform: `translate(-50%, -50%) rotate(${windData.angle + 180}deg)` }} // angle 0 + 180 = 0° pointing down
+                    ></div>
+                    <div className={styles.windSpeed}>
+                        <span>{windData.knots} knots</span> {/* TODO */}
+                        <br />
+                        <span>{windData.angle}°</span> {/* TODO */}
+                    </div>
+                </div>
                 <img src="/images/runway.png" alt="Airstrip" className={styles.airstripImage} />
                 <div className={styles.temperatureContainer}>
                     <div className={styles.temperatureValue}>
@@ -54,7 +80,40 @@ export default function RunwayTemp() {
                         <span>{getTemperatureBySiteId('52437')}</span>
                     </div>
                     <div className={styles.temperatureValue}>
-                        <span>{getTemperatureBySiteId('not in use')}</span>
+                        <span>{getTemperatureBySiteId('52281')}</span>
+                    </div>
+                </div>
+                <div
+                    className={styles.ball}
+                    data-siteid="52280"
+                    style={{ backgroundColor: getColorAndExplanationByCondition(getStationBySiteId('52280')?.condition).color }}
+                >
+                    <div className={styles.tooltip}>
+                        {getStationBySiteId('52280')?.name}<br />
+                        {getColorAndExplanationByCondition(getStationBySiteId('52280')?.condition).explanation}<br />
+                        {getTemperatureBySiteId('52280')}
+                    </div>
+                </div>
+                <div
+                    className={styles.ball}
+                    data-siteid="52437"
+                    style={{ backgroundColor: getColorAndExplanationByCondition(getStationBySiteId('52437')?.condition).color }}
+                >
+                    <div className={styles.tooltip}>
+                        {getStationBySiteId('52437')?.name}<br />
+                        {getColorAndExplanationByCondition(getStationBySiteId('52437')?.condition).explanation}<br />
+                        {getTemperatureBySiteId('52437')}
+                    </div>
+                </div>
+                <div
+                    className={styles.ball}
+                    data-siteid="52281"
+                    style={{ backgroundColor: getColorAndExplanationByCondition(getStationBySiteId('52281')?.condition).color }}
+                >
+                    <div className={styles.tooltip}>
+                        {getStationBySiteId('52281')?.name}<br />
+                        {getColorAndExplanationByCondition(getStationBySiteId('52281')?.condition).explanation}<br />
+                        {getTemperatureBySiteId('52281')}
                     </div>
                 </div>
             </div>
