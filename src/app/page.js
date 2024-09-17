32e373acc8c5693b2485ad5a15ee7ffca12fbe58
SuviAnnina <suvi.varjoranta@gmail.com>
2024-09-17 21:36:35 +0300

    Add adsb data; add map dark mode;

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,42 @@
+"use client";
+
 import RunwayTemp from "@/components/runwayTemp";
 import dynamic from "next/dynamic";
+import { useState, useEffect } from "react";
 
 const Map = dynamic(() => import('@/components/map'), {
   ssr: false
 });
 
 export default function Home() {
+
+  const [flights, setFlights] = useState([]);
+
+  const fetchAdsbData = async () => {
+    try {
+      const response = await fetch('/api/adsb');
+      const result = await response.json();
+
+      if (response.ok && result.length > 0) {
+        setFlights(result);
+      } else {
+        console.warn('ADS-B data response empty/not ok');
+      }
+    }
+    catch (error) {
+      console.error('Error fetching ADS-B data: ', error);
+    }
+  };
+
+
+  useEffect(() => {
+    fetchAdsbData();
+  }, []);
+
   return (
     <div>
       <RunwayTemp />
-      <Map />
+      <Map flights={flights} />
     </div>
   );
 }
--- src/components/map.js
+++ src/components/map.js
@@ -21,16 +21,18 @@ function ResetButton({ initialLocation, initialZoom }) {
                 bottom: '20px',
                 zIndex: 1000,
                 padding: '5px 10px',
-                backgroundColor: '#007bff',
+                backgroundColor: '#fac807',
                 color: 'white',
                 border: 'none',
-                borderRadius: '5px',
+                borderRadius: '30px',
                 cursor: 'pointer',
             }}>
             Reset Map
         </button>
     );
 }
+
+/* Todo: button to swap between dark mode/light mode map layout + icons */
 
 const rotatedIcon = (iconUrl, rotation, iconSize) => {
     const size = iconSize;
@@ -44,12 +46,12 @@ const rotatedIcon = (iconUrl, rotation, iconSize) => {
     });
 };
 
-function Map() {
+function Map({ flights }) {
     const aerodome_location = [60.48075888598088, 26.59665436528449];
     const initial_location = [60.518742, 26.398944];
     const initial_zoom = 7;
 
-    const [iconSize, setIconSize] = useState(32);
+    const [iconSize, setIconSize] = useState(16);
 
     useEffect(() => {
         const handleResize = () => {
@@ -65,16 +67,24 @@ function Map() {
 
     return (
         <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <div style={{ position: 'relative', height: '80%', width: '80%' }}>
+            <div style={{ position: 'relative', height: '80%', width: '80%', borderRadius: '40px', overflow: 'hidden' }}>
                 <MapContainer
                     center={initial_location}
                     zoom={initial_zoom}
                     style={{ height: '100%', width: '100%' }}>
                     <TileLayer
+                        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors & CartoDB'
+                    />{/* Dark mode */}
+
+
+                    {/* <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+                    {/* Light mode */}
+
                     <Marker position={aerodome_location}
-                        icon={rotatedIcon('/svgs/txrunit.svg', 0, iconSize)}>
+                        icon={rotatedIcon('/svgs/txrunit_yellow.svg', 0, iconSize)}>
                         <Tooltip
                             className='custom-tooltip'
                             direction="top"
@@ -85,6 +95,41 @@ function Map() {
                             Helsinki East Aerodome
                         </Tooltip>
                     </Marker>
+
+                    {flights.map((flight) => {
+                        const rotation = flight.trk;
+                        return (
+
+                            <Marker
+                                key={flight.hex}
+                                position={[flight.lat, flight.lon]}
+                                icon={rotatedIcon('/svgs/plane_yellow.svg', rotation, iconSize)}>
+
+                                <Tooltip
+                                    className='custom-tooltip'
+                                    direction="top"
+                                    offset={[0, -12]}
+                                    opacity={1}
+                                    permanent={false} // true: flight number always visible, false: shows while hovered
+                                >
+                                    {flight.fli}
+                                </Tooltip>
+
+                                <Popup>
+                                    <h4 className='popup-h4'>{flight.fli}</h4>
+                                    <div>
+                                        Flight ID: {flight.hex}< br />
+                                        Altitude: {flight.alt} feet< br />
+                                        Speed: {flight.spd}< br />
+                                        Heading: {flight.trk}< br />
+                                        Category: {flight.cat}< br />
+
+                                    </div>
+                                </Popup>
+
+                            </Marker>
+                        );
+                    })}
 
                     <ResetButton initialLocation={initial_location} initialZoom={initial_zoom} />
                 </MapContainer>
