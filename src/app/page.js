43d50436d546428a94276d474c635cd022edc362
Lauri Mäyry <lauri.mayry@gmail.com>
2024-10-02 12:56:30 +0300

    cloudcover

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 "use client";
 
+
+import CloudCover from "@/pages/cloudcover";
 import RunwayTemp from "@/components/runwayTemp";
 import dynamic from "next/dynamic";
 import { useState, useEffect } from "react";
@@ -28,9 +30,27 @@ export default function Home() {
     }
   };
 
+ /* const fetchCloudCoverData = async () => {
+    try {
+      const response = await fetch('/api/cloudcover');
+      const result = await response.json();
+
+      if (response.ok && result.length > 0) {
+        console.log('cloudcover haku toimii')
+      } else {
+        console.warn('Menee vituiksi');
+      }
+    }
+    catch (error) {
+      console.error('Error fetching CLOUDCOVER data: ', error);
+    }
+  };
+*/
 
   useEffect(() => {
     fetchAdsbData();
+    CloudCover();
+    //fetchCloudCoverData();
     const intervalADSB = setInterval(fetchAdsbData, 1000);
     return () => clearInterval(intervalADSB);
   }, []);
---
+++ src/pages/cloudcover.js
@@ -0,0 +1,36 @@
+export default async function CloudCover() {
+    const url = `https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::harmonie::surface::point::multipointcoverage&place=helsinki`;
+    
+    try {
+        const response = await fetch(url, {
+            method: 'POST', // Käytetään POST-metodia
+            headers: {
+                'Content-Type': 'application/xml', // Content-Type XML:lle
+                'Accept': 'application/xml',       // Voit lisätä muita tarvittavia otsikoita tähän
+            }
+        });
+
+        const xmlText = await response.text();
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xmlText, "application/xml");
+
+        // Hakee kaikki <gml:doubleOrNilReasonTupleList> elementit
+        const totalCloudCoverageElements = xmlDoc.getElementsByTagName('gml:doubleOrNilReasonTupleList');
+
+        if (totalCloudCoverageElements.length > 0) {
+            const cloudCoverageRaw = totalCloudCoverageElements[0].textContent.trim();
+            
+            // Jaa rivinvaihtojen mukaan ja ota ensimmäinen arvo
+            const cloudCoverageArray = cloudCoverageRaw.split('\n');
+            const totalCloudCoverage = cloudCoverageArray[0].split(' ')[10]; // Oletetaan, että pilvisyysarvo on toisena
+
+            // Tulostetaan pilvisyysarvo konsoliin
+            console.log(`Total Cloud Coverage: ${totalCloudCoverage}`);
+        } else {
+            console.warn('Pilvisyysarvoja ei löytynyt.');
+        }
+
+    } catch (error) {
+        console.error('Virhe haettaessa XML-tiedostoa:', error);
+    }
+}
